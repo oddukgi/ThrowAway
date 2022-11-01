a60923b6d39617ef06fab31ace42567e1398d9d6
Sunmi Kang <sunmik.dev@gmail.com>
2022-11-01 17:05:40 +0900

    Initial Commit

---
+++ ThrowAway/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ThrowAway
+//
+//  Created by Sally on 2022/11/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
